83741d841f70ca94ada6e65ae1e4014d648bbf21
Mirabbos <mirabbosahmedov7777@gmail.com>
2024-04-19 22:31:05 +0500

    Final task

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int result;
+        int first = number % 10;
+        int second = (number / 10) % 10;
+        int third = number / 100;
+        result = Integer.parseInt(first + "" + second + "" + third);
+        System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
+    public static void main(String[] args) {
+        TemperatureConverter temperatureConverter = new TemperatureConverter();
+        temperatureConverter.toFahrenheit(25);
+    }
     public void toFahrenheit(int temperatureCelsius){
+        double temperatureFahrenheit = (9.0 / 5.0) * temperatureCelsius + 32;
+        System.out.println(temperatureFahrenheit);
     }
 }
